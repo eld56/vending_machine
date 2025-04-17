c3a5fe0e30a7b089759c64b37162a94d64251f61
eld56 <jumgjust@gmail.com>
2025-04-17 16:00:28 +0600

    I make MonyeReaceiver

--- src/model/MoneyReaceiver.java
+++ src/model/MoneyReaceiver.java
@@ -1,4 +1,17 @@
 package model;
 
 public class MoneyReaceiver {
+    private int money;
+
+    public MoneyReaceiver(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
